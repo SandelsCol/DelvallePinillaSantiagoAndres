9cc99bf09498b42087a240c7083751877b0fd260
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-15 20:53:49 -0500

    Ejercicio 2

--- SantiagoDelValle.go
+++ SantiagoDelValle.go
@@ -1,9 +1,7 @@
 package main
 
 func main() {
-	var Promedio_compañia_1, Promedio_compañia_2, diferencia int
-	Promedio_compañia_1 := (27834 + 23789) / 2
-	Promedio_compañia_2 := (834 + 4614) / 2
-	diferencia := Promedio_compañia_1 - Promedio_compañia_2
-	println(diferencia)
+	median := 29011.5
+	println("La mediana es", median)
+
 }
