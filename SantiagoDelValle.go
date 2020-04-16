e5477fc3d5adeb32233f90951ed55d9aaa545b65
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-15 21:28:06 -0500

    Ejercicio7

--- SantiagoDelValle.go
+++ SantiagoDelValle.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	deficit  := 4614 - 834
-	deficit2 := -(deficit)
+	Año2008 := 11.984551063729015
+	Año2009 := 10.242885868184578
+	Año2010 := 12.998548971147347
+	Año2011 := 13.333534267101257
+	Año2012 := 13.994032267092646
+	Año2013 := 14.080146739060233
+	Año2014 := 13.634073774268135
+	Año2015 := 7.386468833019733
+	Año2016 := 1.9866608682922209
+	Año2017 := 0.35909734810483573
 
-	println("El deficit fue de ",deficit,",Es decir, se perdieron,",deficit2",Millones de pesos colombianos)
+	Deficit1 := Año2009 - Año2008
+	Deficit2 := Año2010 - Año2009
+	Deficit3 := Año2011 - Año2010
+	Deficit4 := Año2012 - Año2011
+	Deficit5 := Año2013 - Año2012
+	Deficit6 := Año2014 - Año2013
+	Deficit7 := Año2015 - Año2014
+	Deficit8 := Año2016 - Año2015
+	deficit9 := Año2017 - Año2016
 
+	println("Los proximos valores corresponden a a la alza o baja de la empresa durante los años 2008 al 2017")
+
+	println(Deficit1, Deficit2, Deficit3, Deficit4, Deficit5, Deficit6, Deficit7, Deficit8, deficit9)
 }
